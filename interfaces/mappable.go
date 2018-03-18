478b6f28731387ec99f5068dab776994d31e04fa
naoto0822 <n.h.in.m.h@gmail.com>
2018-03-18 23:31:23 +0900

    add comment for lint

--- interfaces/mappable.go
+++ interfaces/mappable.go
@@ -1,5 +1,7 @@
 package interfaces
 
+// Mappable is converted to gss.Feed
 type Mappable interface {
+	// ToJSON convert gss.Feed
 	ToJSON() ([]byte, error)
 }
--- interfaces/parseable.go
+++ interfaces/parseable.go
@@ -1,5 +1,7 @@
 package interfaces
 
+// Parseable is Parser interface for feed
 type Parseable interface {
+	// Parse parsing XML byte and return struct implmented interfaces.Mappable
 	Parse([]byte) (*Mappable, error)
 }
